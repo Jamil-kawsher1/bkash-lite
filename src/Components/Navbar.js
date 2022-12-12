2e5286c821f3ec81b599e6827a32f1d517d0a9f6
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2022-12-12 17:06:01 +0600

    First Commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,138 @@
+import React, { useEffect, useRef, useState } from "react";
+import "../Style/navbar.css";
+import bkash from "../img/BKash-Icon2-Logo.wine.svg";
+import { Link, NavLink } from "react-router-dom";
+import { AiOutlineMenuFold } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
+const Navbar = () => {
+  const [isMenueOpen, setIsMenuOpen] = useState(false);
+  const [eng, setEng] = useState(false);
+  const handleMenuOpen = () => {
+    setIsMenuOpen(!isMenueOpen);
+  };
+  useEffect(() => {
+    // document.addEventListener("click", handleOutsideMenuClick);
+  }, []);
+  const mobileMenuRef = useRef();
+  const menucloseRef = useRef();
+
+  const handleOutsideMenuClick = (e) => {
+    if (
+      !mobileMenuRef.current.contains(e.target) &&
+      !menucloseRef.current.contains(e.target)
+    ) {
+      console.log("clicked out side");
+    } else {
+      console.log("Clicked Inside");
+    }
+  };
+
+  // const handleLanguage = () => {};
+  return (
+    <div className="nav-view">
+      <nav>
+        <div className="container navbar-content">
+          <div className="logo">
+            {eng ? <span>bkash</span> : <span> বিকাশ</span>}
+            <img src={bkash} alt="" />
+          </div>
+          <div className="nav-link">
+            <NavLink className="common-nav-link" to="/campaign">
+              {eng ? "Campaigns" : "ক্যাম্পেইন"}
+            </NavLink>
+            <NavLink className="common-nav-link" to="/services">
+              {eng ? "Services" : " সার্ভিসেস"}
+            </NavLink>
+            <NavLink className="common-nav-link" to="/business">
+              {eng ? "Business" : " বিজনেস"}
+            </NavLink>
+            <NavLink className="common-nav-link" to="/help">
+              {eng ? "Help" : " হেল্প"}
+            </NavLink>
+            <NavLink className="common-nav-link" to="/carrer">
+              {eng ? "Career" : " ক্যারিয়ার"}
+            </NavLink>
+            <NavLink className="common-nav-link" to="/about">
+              {eng ? "About" : " পরিচিতি"}
+            </NavLink>
+          </div>
+          <div className="nav-other">
+            <button className="btn-nav">
+              {eng ? <span>bkash App</span> : <span>বিকাশ এ্যাপ</span>}
+            </button>
+            <span className="lang-select">
+              <span
+                className={eng && "lang-select-bold"}
+                onClick={() => setEng(true)}
+              >
+                ENG
+              </span>{" "}
+              |
+              <span
+                className={!eng && "lang-select-bold"}
+                onClick={() => setEng(false)}
+              >
+                {" "}
+                বাংলা
+              </span>
+            </span>
+            <span onClick={handleMenuOpen} className="menu-icon">
+              <span className="menu-icon-text">মেনু</span> <AiOutlineMenuFold />
+            </span>
+
+            <div></div>
+          </div>
+        </div>
+      </nav>
+      {isMenueOpen && (
+        <div ref={mobileMenuRef} className="nav-mobile-menu">
+          <div className="menu-close">
+            <div className="close-btn-slogan">
+              <AiOutlineClose onClick={handleMenuOpen} />
+              <div>
+                <span className="brand-f-letter">বি</span>কাশ
+              </div>
+            </div>
+
+            <div className="mobile-menu-links-collection">
+              <Link className="mobile-menu-links" to="/about">
+                {eng ? "Campaigns" : "ক্যাম্পেইন"}
+                <hr className="mobile-link-hr" />
+              </Link>
+
+              <Link className="mobile-menu-links" to="/about">
+                সার্ভিসেস
+                <hr className="mobile-link-hr" />
+              </Link>
+              <Link className="mobile-menu-links" to="/about">
+                বিজনেস
+                <hr className="mobile-link-hr" />
+              </Link>
+              <Link className="mobile-menu-links" to="/about">
+                হেল্প
+                <hr className="mobile-link-hr" />
+              </Link>
+              <Link className="mobile-menu-links" to="/about">
+                ক্যারিয়ার
+                <hr className="mobile-link-hr" />
+              </Link>
+              <Link className="mobile-menu-links" to="/about">
+                পরিচিতি
+                <hr className="mobile-link-hr" />
+              </Link>
+              <Link className="mobile-menu-links" to="/about">
+                ব্লগ
+                <hr className="mobile-link-hr" />
+              </Link>
+            </div>
+            <button className="bkash-btn-mobile-menu">
+              {eng ? <span>bkash App</span> : <span>বিকাশ এ্যাপ</span>}
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Components/Slider.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import bkcover1 from "../img/bkcover1.png";
+import bkcover2 from "../img/bk2cover.png";
+import bkcover3 from "../img/bk3cover.png";
+import "../Style/designSystem.css";
+import Navbar from "./Navbar";
+import { Autoplay, Navigation } from "swiper";
+const Slider = () => {
+  return (
+    <div className="slider-view">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        autoplay={{
+          delay: 2000,
+        }}
+        navigation={true}
+        modules={[Navigation, Autoplay]}
+        onSlideChange={() => console.log("slide change")}
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide className="slider-view">
+          <img className="responsive-img" src={bkcover1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className="slider-view">
+          <img className="responsive-img" src={bkcover2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className="slider-view">
+          <img className="responsive-img" src={bkcover3} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Slider;
